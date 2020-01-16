6198adec9d915813d185aa1132972e972adaf53c
Michael Sogos <michael.sogos@gurustudioweb.it>
2020-01-16 09:45:11 +0100

    * Fixed not importable module in nodejs

--- src/index.js
+++ src/index.js
@@ -3,34 +3,38 @@ const compareApi = require("./api/CompareApi");
 const EventEmitter = require("events");
 
 class PgDiff {
-    constructor(config) {
-        this["config"] = config;
-        /** @type {import("events")} */
+  /**
+   *
+   * @param {import("./models/config")} config
+   */
+  constructor(config) {
+    this["config"] = config;
+    /** @type {import("events")} */
 
-        this.events = new EventEmitter();
-    }
+    this.events = new EventEmitter();
+  }
 
-    /**
-     *
-     * @param {Boolean} force
-     * @returns {Promise<import("./models/patchInfo")[]>} Return a list of PatchInfo.
-     */
-    async migrate(force) {
-        force = force || false;
-        return await migrationApi.migrate(this.config, force);
-    }
+  /**
+   *
+   * @param {Boolean} force
+   * @returns {Promise<import("./models/patchInfo")[]>} Return a list of PatchInfo.
+   */
+  async migrate(force) {
+    force = force || false;
+    return await migrationApi.migrate(this.config, force);
+  }
 
-    /**
-     *
-     * @param {String} scriptName
-     * @returns {String} Return null if no patch has been created.
-     */
-    async compare(scriptName) {
-        if (!scriptName) throw new Error("The script name must be specified!");
-        return await compareApi.compare(this.config, scriptName, this.events);
-    }
+  /**
+   *
+   * @param {String} scriptName
+   * @returns {String} Return null if no patch has been created.
+   */
+  async compare(scriptName) {
+    if (!scriptName) throw new Error("The script name must be specified!");
+    return await compareApi.compare(this.config, scriptName, this.events);
+  }
 
-    /*
+  /*
      async savePatch(config, patchFileName) {
         let migrationConfig = core.prepareMigrationConfig(config);
         let pgClient = await core.makePgClient(config.targetClient);
