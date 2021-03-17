a423001015608447c679af3fb2fe26518e05abba
Rio6 <rio.liu@r26.me>
2021-03-17 15:59:08 -0400

    Change typo occure -> occur

--- src/sqlScriptGenerator.js
+++ src/sqlScriptGenerator.js
@@ -1,13 +1,13 @@
 const hints = {
 	addColumnNotNullableWithoutDefaultValue:
-		" --WARN: Add a new column not nullable without a default value can occure in a sql error during execution!",
+		" --WARN: Add a new column not nullable without a default value can occur in a sql error during execution!",
 	changeColumnDataType:
-		" --WARN: Change column data type can occure in a casting error, the suggested casting expression is the default one and may not fit your needs!",
-	dropColumn: " --WARN: Drop column can occure in data loss!",
+		" --WARN: Change column data type can occur in a casting error, the suggested casting expression is the default one and may not fit your needs!",
+	dropColumn: " --WARN: Drop column can occur in data loss!",
 	potentialRoleMissing:
-		" --WARN: Grant\\Revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!",
-	identityColumnDetected: " --WARN: Identity column has been detected, an error can occure because constraints violation!",
-	dropTable: " --WARN: Drop table can occure in data loss!",
+		" --WARN: Grant\\Revoke privileges to a role can occur in a sql error during execution if role is missing to the target database!",
+	identityColumnDetected: " --WARN: Identity column has been detected, an error can occur because constraints violation!",
+	dropTable: " --WARN: Drop table can occur in data loss!",
 };
 
 var helper = {
