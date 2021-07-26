556ccfd96fe750d72cb5bec40df1d529c7437d9e
Thiago Brandão Damasceno <thiagobrandam@gmail.com>
2021-07-26 12:02:57 -0300

    Fix view's dependency column name comparison
    Dependencies return strings in the `rawColumnName` format

--- src/api/CompareApi.js
+++ src/api/CompareApi.js
@@ -443,7 +443,7 @@ class CompareApi {
 			for (let view in dbTargetObjects.views) {
 				dbTargetObjects.views[view].dependencies.forEach((dependency) => {
 					let fullDependencyName = `"${dependency.schemaName}"."${dependency.tableName}"`;
-					if (fullDependencyName == tableName && dependency.columnName == columnName) {
+					if (fullDependencyName == tableName && dependency.columnName == rawColumnName) {
 						sqlScript.push(sql.generateDropViewScript(view));
 						droppedViews.push(view);
 					}
@@ -454,7 +454,7 @@ class CompareApi {
 			for (let view in dbTargetObjects.materializedViews) {
 				dbTargetObjects.materializedViews[view].dependencies.forEach((dependency) => {
 					let fullDependencyName = `"${dependency.schemaName}"."${dependency.tableName}"`;
-					if (fullDependencyName == tableName && dependency.columnName == columnName) {
+					if (fullDependencyName == tableName && dependency.columnName == rawColumnName) {
 						sqlScript.push(sql.generateDropMaterializedViewScript(view));
 						droppedViews.push(view);
 					}
